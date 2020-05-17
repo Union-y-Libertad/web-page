46515f4b140d523c32e2459db272139123aced0f
marce1994 <>
2020-05-17 04:49:20 +0200

    first commit

---
+++ app/src/App.svelte
@@ -0,0 +1,40 @@
+
+<TopAppBar {dense} {prominent} {variant} color='secondary'>
+  <Row>
+    <Section>
+      <IconButton class="material-icons">menu</IconButton>
+      <Title>Union y libertad</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton class="material-icons" aria-label="Download">file_download</IconButton>
+      <IconButton class="material-icons" aria-label="Print this page">print</IconButton>
+      <IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+    </Section>
+  </Row>
+</TopAppBar>
+<div use:Adjust>
+  <Home></Home>
+  <Referentes></Referentes>
+  <MyFooter></MyFooter>
+</div>
+
+<script>
+  let dense = false;
+  let prominent = true;
+  let variant = 'standard';
+
+  import TopAppBar, {Row, Section, Title, FixedAdjust, DenseFixedAdjust, ProminentFixedAdjust, ShortFixedAdjust} from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+  import Checkbox from '@smui/checkbox';
+  import FormField from '@smui/form-field';
+  import LinearProgress from './components/linear-progress.svelte';
+  import Referentes from './components/referentes.svelte';
+  import MyFooter from './components/footer.svelte';
+  import Home from './components/home.svelte';
+
+  let Adjust = ProminentFixedAdjust;
+</script>
+
+<style>
+
+</style>
---
+++ app/src/components/referentes.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Card, {
+    Content,
+    PrimaryAction,
+    Media,
+    MediaContent,
+    Actions,
+    ActionButtons,
+    ActionIcons
+  } from "@smui/card";
+  import Button, { Label } from "@smui/button";
+  import IconButton, { Icon } from "@smui/icon-button";
+  import List, { Item, Text } from "@smui/list";
+  import Grid from "svelte-grid-responsive";
+
+  function doAction(action) {
+    alert("You did an action: " + action);
+  }
+</script>
+
+<section id="referentes" gutter="12">
+
+<h1>Referentes</h1>
+  <div>
+    <h2>Tipito enojado</h2>
+    <img src="https://yt3.ggpht.com/a/AATXAJx-k8hO9-6NQJB3qQw9KIB0Kn5hEtfhmuF6mA=s100-c-k-c0xffffffff-no-rj-mo" alt="tipito-logo"/>
+    <p>
+      Contenido del parrafo/ descripcion
+    </p>
+    <p>
+      Segundo parrafo de la descripcion.
+    </p>
+  </div>
+</section>
